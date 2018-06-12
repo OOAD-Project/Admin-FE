0c88e556e770b43b3c3775738047e4a3d090606e
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-06-12 14:25:33 +0800

    init project

---
+++ .webpackrc.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+  ],
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+    },
+    production: {
+      define: {
+        'BASE_URL': '/Admin-FE/'
+      },
+      publicPath: '/Admin-FE/static/'
+    }
+  },
+  alias: {
+    components: path.resolve(__dirname, 'src/components/'),
+  },
+  ignoreMomentLocale: true,
+  theme: './src/theme.js',
+  html: {
+    template: './src/index.ejs',
+  },
+  disableDynamicImport: true,
+  publicPath: '/',
+  hash: true,
+};
