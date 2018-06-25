892166d72dfa44bbe57b5d8b47c088be90d7fca6
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-06-26 01:13:49 +0800

    登录页面

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -9,25 +9,25 @@ import { getRoutes } from '../utils/utils';
 
 const links = [
   {
+    key: 'org',
+    title: '组织',
+    href: 'https://github.com/OOAD-Project',
+  },
+  {
+    key: 'project',
+    title: '项目地址',
+    href: 'https://github.com/OOAD-Project/Admin-FE',
+  },
+  {
     key: 'help',
-    title: '帮助',
-    href: '',
-  },
-  {
-    key: 'privacy',
-    title: '隐私',
-    href: '',
-  },
-  {
-    key: 'terms',
-    title: '条款',
-    href: '',
+    title: '帮助说明',
+    href: 'https://ooad-project.github.io/DashBoard/',
   },
 ];
 
 const copyright = (
   <Fragment>
-    Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
+    Copyright <Icon type="copyright" /> 2018 OOAD-Project
   </Fragment>
 );
 
@@ -37,7 +37,7 @@ class UserLayout extends React.PureComponent {
     const { pathname } = location;
     let title = 'Ant Design Pro';
     if (routerData[pathname] && routerData[pathname].name) {
-      title = `${routerData[pathname].name} - Ant Design Pro`;
+      title = `${routerData[pathname].name} - EasyOrder`;
     }
     return title;
   }
@@ -50,11 +50,10 @@ class UserLayout extends React.PureComponent {
             <div className={styles.top}>
               <div className={styles.header}>
                 <Link to="/">
-                  <img alt="logo" className={styles.logo} src={logo} />
-                  <span className={styles.title}>Ant Design</span>
+                  <span className={styles.title}>Easy Order</span>
                 </Link>
               </div>
-              <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+              <div className={styles.desc}>Easy Order Admin 后台管理系统</div>
             </div>
             <Switch>
               {getRoutes(match.path, routerData).map(item => (
